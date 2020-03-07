7c420016ad397ee227991d36ce3b23b662c254d8
Peter <47712814+PeterPavlovIvanov@users.noreply.github.com>
2020-03-07 20:30:58 +0200

    Delete LabelRepository.java

--- spring-advanced-query/src/main/java/dao/LabelRepository.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.awt.*;
-
-public interface LabelRepository extends JpaRepository<Label,Long> {
-}
