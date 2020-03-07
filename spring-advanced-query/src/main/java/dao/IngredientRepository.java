f281da5acda83d52fc9e445b6bc23ff50e585448
Peter <47712814+PeterPavlovIvanov@users.noreply.github.com>
2020-03-07 20:30:45 +0200

    Delete IngredientRepository.java

--- spring-advanced-query/src/main/java/dao/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import entity.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface IngredientRepository extends JpaRepository<Ingredient,Long> {
-
-}
